6596cbb355fe16db9d9067ed4e198d46d8fbfd99
sam l <sluongo718@yahoo.com>
2020-09-18 03:29:48 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,17 @@
-module Memorable 
-  
-  
-  def reset_all
-    self.all.clear
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
   end
-
-  def count
-    self.all.count
+ 
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
-  
-  
 end
